499d330029a8922d144a5052c7d69776d82cb5b1
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-03-07 17:01:59 +0100

    Filtering overlapping shapelets for post-processing

--- python/postprocess_shapelets.py
+++ python/postprocess_shapelets.py
@@ -10,6 +10,9 @@ import json
 import math
 import sys
 
+import numpy    as np
+import networkx as nx
+
 def overlaps(i1,s1,l1,i2,s2,l2):
   """
   Checks whether two shapelets overlap (with respect to the time series
@@ -34,15 +37,33 @@ if __name__ == "__main__":
   shapelets = data["shapelets"]
   n         = len(shapelets)
   intervals = []
+  p_values  = []
 
   for shapelet in shapelets:
+    p = shapelet["p_val"]
     i = shapelet["index"]
     s = shapelet["start"]
     l = len(shapelet["shapelet"])
 
     intervals.append( (i,s,l) )
+    p_values.append(p)
+
+  G = nx.Graph()
+  G.add_nodes_from(range(n))
 
   for i in range(n):
     for j in range(i+1,n):
       if overlaps(*intervals[i], *intervals[j]):
-        print(*intervals[i], *intervals[j])
+        G.add_edge(i,j)
+
+  components = list(nx.connected_components(G))
+  for component in components:
+    min_index = -1
+    min_p     = np.Inf
+
+    for index in component:
+      if p_values[index] < min_p:
+        min_index = index
+        min_p     = p_values[index]
+
+    print("{0:4d}: {1:0.8e} {2}".format(min_index, min_p, shapelets[min_index]["shapelet"]))
