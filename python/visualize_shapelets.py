7d8165d7b1e0a9c41c6a8ff0381a0b37c799fbf9
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-03-07 13:11:46 +0100

    Opacity mapping for shapelet visualization

--- python/visualize_shapelets.py
+++ python/visualize_shapelets.py
@@ -7,6 +7,7 @@
 # Output: None
 
 import json
+import math
 import sys
 
 import numpy             as np
@@ -17,9 +18,24 @@ if __name__ == "__main__":
     data = json.load(f)
 
   shapelets = data["shapelets"]
+  p_values  = []
 
   for shapelet in shapelets:
-    s = shapelet["shapelet"]
-    plt.plot(s)
+    p_values.append( shapelet["p_val"] )
+
+  p_min = math.log( min(p_values) )
+  p_max = math.log( max(p_values) )
+
+  for shapelet in shapelets:
+    s     = shapelet["shapelet"]
+    p     = shapelet["p_val"]
+    p     = math.log(p)
+
+    if p_max != p_min:
+      alpha = max( (p_max - p) / (p_max - p_min), 0.05 )
+    else:
+      alpha = 1.0
+
+    plt.plot(s, 'k', alpha=alpha, linewidth=5*alpha)
 
   plt.show()
