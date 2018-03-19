dbb85a74c0f08601dc67ab4ea3c73b2c4e2e5f57
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-03-19 11:42:21 +0100

    Demo script for time series variability analysis

---
+++ code/python/analyse_variability.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+#
+# analyse_variability.py: Given a data set, analysis the variability of
+# all time series. Variability is defined as differences along the time
+# axis.
+#
+# Input:  CSV file with time series
+# Output: Set of "variability time series"
+#
+# The output will be written to `stdout`.
+
+import argparse
+import csv
+import sys
+
+import numpy as np
+
+def variability(time_series):
+  """
+  Calculates the variability of a given time series.
+  """
+
+  data = []
+  for index, y in enumerate(time_series):
+    if index > 0:
+      data.append( y - time_series[index-1] )
+
+  return data
+
+def format_series(time_series):
+  """
+  Formats a time series as a string for output.
+  """
+
+  data = ""
+  for x,y in enumerate(time_series):
+    data += "{}\t{}\n".format(x,y)
+
+  data += "\n"
+  return data
+
+if __name__ == "__main__":
+  
+  time_series = []
+  labels      = []
+
+  parser = argparse.ArgumentParser(description="Time Series Variability Analysis")
+
+  parser.add_argument("input",
+    metavar  = "INPUT",
+    help     = "Input file")
+
+  arguments  = parser.parse_args()
+  input_file = arguments.input
+
+  with open(input_file) as f:
+    reader = csv.reader(f)
+    for row in reader:
+
+      # Skip comments; some versions of our data files make use of them
+      # in order maintain provenance information.
+      if row[0].startswith("#"):
+        continue
+
+      time_series.append( [ float(x) for x in row[1:] ] )
+      labels.append( float(row[0]) )
+
+  cases    = [ series for index, series in enumerate(time_series) if labels[index] == 1.0 ]
+  controls = [ series for index, series in enumerate(time_series) if labels[index] != 1.0 ]
+
+  for series in cases:
+    sys.stdout.write(format_series(variability(series)))
+
+  sys.stdout.write("\n\n")
+
+  for series in controls:
+    sys.stdout.write(format_series(variability(series)))
