79900d7367044bbf8ae3be85e3fde76f5c7dd4e5
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-07-27 08:43:29 +0200

    Using environment file to search for data

--- code/cpp/tests/test_time_series_reading.cc
+++ code/cpp/tests/test_time_series_reading.cc
@@ -1,5 +1,8 @@
+#include <iostream>
+
 #include <cassert>
 
+#include "Environment.hh"
 #include "TimeSeries.hh"
 #include "Utilities.hh"
 
@@ -8,8 +11,10 @@ int main( int, char** )
   // Default settings for reading --------------------------------------
 
   {
-    auto data         = readData( "./data/short.csv" );
+    auto data         = readData( CMAKE_SOURCE_DIR + std::string("/tests/data/short.csv") );
     auto&& timeSeries = data.first;
+
+    std::cerr << timeSeries.size() << "\n";
 
     assert( timeSeries.size() == 4 );
   }
