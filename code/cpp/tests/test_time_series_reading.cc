5da2d129b2dfa50d81f51e2ccea50ea850434113
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-07-27 08:31:42 +0200

    Updated test for time series reading
    Need a way to access the test directory in order to read data
    automatically. I could even use the source directory for this.

--- code/cpp/tests/test_time_series_reading.cc
+++ code/cpp/tests/test_time_series_reading.cc
@@ -1,5 +1,16 @@
 #include <cassert>
 
+#include "TimeSeries.hh"
+#include "Utilities.hh"
+
 int main( int, char** )
 {
+  // Default settings for reading --------------------------------------
+
+  {
+    auto data         = readData( "./data/short.csv" );
+    auto&& timeSeries = data.first;
+
+    assert( timeSeries.size() == 4 );
+  }
 }
