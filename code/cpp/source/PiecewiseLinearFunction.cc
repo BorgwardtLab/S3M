8fa37758bbc6a6ed8cc4403428248cf1ba841e5b
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-01-10 09:09:46 +0100

    Implemented simple operators
    For now, this *only* works for time series that have been sampled at the
    same time points. Moreover, the time series need to have the same length
    but this is luckily a given for our input data.

--- code/cpp/source/PiecewiseLinearFunction.cc
+++ code/cpp/source/PiecewiseLinearFunction.cc
@@ -1,11 +1,15 @@
 #include "PiecewiseLinearFunction.hh"
 
+#include <functional>
+
 PiecewiseLinearFunction PiecewiseLinearFunction::operator+( const PiecewiseLinearFunction& other ) const
 {
+  return this->apply( other, std::plus<ValueType>() );
 }
 
 PiecewiseLinearFunction PiecewiseLinearFunction::operator-( const PiecewiseLinearFunction& other ) const
 {
+  return this->apply( other, std::minus<ValueType>() );
 }
 
 bool PiecewiseLinearFunction::hasSameDomain( const PiecewiseLinearFunction& ) const
