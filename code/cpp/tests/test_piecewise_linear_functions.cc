09ffac5ade7f36b325fa01da3835dec757f59b41
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-01-10 10:22:44 +0100

    Basic test for norm calculation

--- code/cpp/tests/test_piecewise_linear_functions.cc
+++ code/cpp/tests/test_piecewise_linear_functions.cc
@@ -9,4 +9,12 @@ int main( int, char** )
   auto f = PiecewiseLinearFunction( {0.0, 1.0, 0.0} );
   auto g = PiecewiseLinearFunction( {0.0, 0.5, 0.0} );
   auto h = f - f;
+
+  assert( f.length() == 3);
+  assert( f.length() == g.length() );
+  assert( f.length() == h.length() );
+
+  assert( f.norm(1) == 1.00 );
+  assert( g.norm(1) == 0.50 );
+  assert( h.norm(1) == 0.00 );
 }
