6defb031e89bc2f0a1d760afbf4cdf3cacf04769
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-01-14 17:17:38 +0100

    Added distance functor skeleton

---
+++ code/cpp/include/DistanceFunctor.hh
@@ -0,0 +1,31 @@
+#ifndef DISTANCE_FUNCTOR_HH__
+#define DISTANCE_FUNCTOR_HH__
+
+// Using a forward reference because it is sufficient here, thus
+// minimizing the complexity of all dependencies to some extent.
+class TimeSeries;
+
+class DistanceFunctor
+{
+public:
+
+  // Forwards the return value type of this functor to make it easier to
+  // remain type-agnostic.
+  using ValueType = double;
+
+  // A default constructor is sufficient, as this class is not supposed
+  // to carry any significant amounts of data.
+  DistanceFunctor() = default;
+
+  // Ditto for the destructor.
+  virtual ~DistanceFunctor() = default;
+
+  /**
+    Main entry point for the functor. This calculates the distance
+    between two time series.
+  */
+
+  virtual ValueType operator()( const TimeSeries& S, const TimeSeries& T ) const = 0; 
+};
+
+#endif
